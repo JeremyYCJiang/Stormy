d738faa0373dc8adfe5de751e9286f70d6ad41a0
JeremyYCJiang <inopp111@icloud.com>
2015-04-04 16:19:24 +0800

    Remove unused logs and annotations

--- app/src/main/java/com/jiangziandroid/stormy/MainActivity.java
+++ app/src/main/java/com/jiangziandroid/stormy/MainActivity.java
@@ -41,24 +41,15 @@ public class MainActivity extends ActionBarActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Create the API to Forecast Server
-        //double latitude = 65.9667;//37.8267;
-        //double longitude = -18.5333;//-122.423;
-
         if(isNetworkAvailable()) {
             buildGoogleApiClient();
-
         }
         else {
             Toast.makeText(this, getString(R.string.network_unavailable_message), Toast.LENGTH_LONG).show();
         }
-
-
         Toast.makeText(this, "Main UI code is running!", Toast.LENGTH_LONG).show();
-        //Log.d(TAG, "Main UI code is running!");
-
     }
+
 
     private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
             JSONObject forecast = new JSONObject(jsonData);
@@ -74,20 +65,9 @@ public class MainActivity extends ActionBarActivity{
             currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
             currentWeather.setSummary(currently.getString("summary"));
             currentWeather.setTimeZone(forecast.getString("timezone"));
-            Log.d(TAG, currentWeather.getFormattedTime());
-            Log.d(TAG, String.valueOf(currentWeather.getTemperature()));
-            Log.d(TAG, String.valueOf(currentWeather.getCelsiusTemperature()));
             return currentWeather;
-
-            /*
-            String icon = currently.getString("icon");
-            long time = currently.getLong("time");
-            double temperature = currently.getDouble("temperature");
-            double humidity = currently.getDouble("humidity");
-            double precipProbability = currently.getDouble("precipProbability");
-            String summary = currently.getString("summary");
-            */
     }
+
 
     private boolean isNetworkAvailable() {
         ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -98,6 +78,7 @@ public class MainActivity extends ActionBarActivity{
         }
         return isAvailable;
     }
+
 
     private void alertUserAboutError() {
         AlertDialogFragment dialog = new AlertDialogFragment();
@@ -110,9 +91,9 @@ public class MainActivity extends ActionBarActivity{
      */
     protected synchronized void buildGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
-                    @Override
-                    public void onConnected(Bundle connectionHint) {
+        .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+            @Override
+            public void onConnected(Bundle connectionHint) {
                         // Provides a simple way of getting a device's location and is well suited for
                         // applications that do not require a fine-grained location and that do not need location
                         // updates. Gets the best and most recent location currently available, which may be null
@@ -121,7 +102,6 @@ public class MainActivity extends ActionBarActivity{
                         if (mLastLocation != null) {
                             latitude = mLastLocation.getLatitude();
                             longitude = mLastLocation.getLongitude();
-
                             Toast.makeText(MainActivity.this, "mGoogleApiClient Connected.", Toast.LENGTH_LONG).show();
                             TextView latitudeTextView = (TextView) findViewById(R.id.locationLabel);
                             latitudeTextView.setText(String.valueOf(latitude));
@@ -130,14 +110,12 @@ public class MainActivity extends ActionBarActivity{
 
                             OkHttpClient client = new OkHttpClient();
                             Toast.makeText(MainActivity.this, "Initializing okhttp client...", Toast.LENGTH_LONG).show();
-
                             Request request = new Request.Builder().url(forecastUrl).build();
                             Call call = client.newCall(request);
                             //Transfer synchronous to asynchronous
                             call.enqueue(new Callback() {
                                 @Override
                                 public void onFailure(Request request, IOException e) {
-
                                 }
 
                                 @Override
@@ -161,36 +139,32 @@ public class MainActivity extends ActionBarActivity{
                                     }
                                 }
                             });
-
                         } else {
                             Toast.makeText(MainActivity.this, "No location detected", Toast.LENGTH_LONG).show();
                         }
                     }
-
-
-                    @Override
-                    public void onConnectionSuspended(int cause) {
+            @Override
+            public void onConnectionSuspended(int cause) {
                         // The connection to Google Play services was lost for some reason. We call connect() to
                         // attempt to re-establish the connection.
                         Toast.makeText(MainActivity.this, "Connection suspended", Toast.LENGTH_LONG).show();
                         mGoogleApiClient.connect();
-
                     }
-                })
-                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+             })
+            .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
                     @Override
                     public void onConnectionFailed(ConnectionResult result) {
-                        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+                  // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
                         // onConnectionFailed.
-                        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
-                        Toast.makeText(MainActivity.this, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode(),
-                                Toast.LENGTH_LONG).show();
-                        Dialog dialog = GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), MainActivity.this, 1);
-                        dialog.show();
+                  Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+                  Toast.makeText(MainActivity.this, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode(),
+                    Toast.LENGTH_LONG).show();
+                  Dialog dialog = GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), MainActivity.this, 1);
+                  dialog.show();
                     }
-                })
-                .addApi(LocationServices.API)
-                .build();
+             })
+            .addApi(LocationServices.API)
+            .build();
         Log.i(TAG, "mGoogleApiClient initializing...");
         Toast.makeText(this, "mGoogleApiClient initializing...", Toast.LENGTH_LONG).show();
         mGoogleApiClient.connect();
